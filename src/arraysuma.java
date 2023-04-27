855042bd3b4d0d01dc8b26f3dff1189f97e496b6
Adolfo Sánchez Lopéz <81250601+Adolfsan99@users.noreply.github.com>
2023-04-27 13:50:10 -0500

    Add files via upload

---
+++ src/arraysuma.java
@@ -0,0 +1,13 @@
+public class arraysuma {
+    public static void main(String[] args) {
+
+        int[] arr = new int[3];
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        System.out.println(arr[0]+arr[1]+arr[2]);
+
+}
+}
